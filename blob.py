7fe375da745e86ec83b62385d19c1da97de05541
Max Schommer <maxschommer@gmail.com>
2018-01-22 19:57:40 -0500

    Got weld tip angle working mostly

--- blob.py
+++ blob.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np;
 import matplotlib.pyplot as plt
 from skimage.measure import compare_ssim as ssim
-
+from sklearn.cluster import MiniBatchKMeans
 sift = cv2.xfeatures2d.SIFT_create()
 MIN_MATCH_COUNT = 5
 
@@ -13,11 +13,61 @@ def mse(imageA, imageB):
     # NOTE: the two images must have the same dimension
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
-    
+
     # return the MSE, the lower the error, the more "similar"
     # the two images are
     return err
 
+def detectLines(imageEdges):
+    angleThreshold = .1
+    expectedAngle = 1.4
+    # This returns an array of r and theta values
+    lines = cv2.HoughLines(imageEdges,1,np.pi/180, 80)
+    # The below for loop runs till r and theta values 
+    # are in the range of the 2d array
+    welderAngle = []
+    for lineVals in lines:
+        r, theta = lineVals[0]
+        welderAngle.append(theta)
+        # if ((np.abs(theta) < angleThreshold) |\
+        #  ((np.abs(theta) < np.pi/2 + angleThreshold) & (np.abs(theta) > np.pi/2 - angleThreshold)) |\
+        #  ((np.abs(theta) < 3*np.pi/2 + angleThreshold) & (np.abs(theta) > 8*np.pi/2 - angleThreshold))):
+        #     print(theta)
+        #     continue
+        if (np.abs(theta) < expectedAngle - 2*angleThreshold) | (np.abs(theta) > expectedAngle + angleThreshold):
+            continue
+        # Stores the value of cos(theta) in a
+        a = np.cos(theta)
+     
+        # Stores the value of sin(theta) in b
+        b = np.sin(theta)
+         
+        # x0 stores the value rcos(theta)
+        x0 = a*r
+
+        # y0 stores the value rsin(theta)
+        y0 = b*r
+
+        # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
+        x1 = int(x0 + 1000*(-b))
+
+        # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
+        y1 = int(y0 + 1000*(a))
+
+        # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
+        x2 = int(x0 - 1000*(-b))
+
+        # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
+        y2 = int(y0 - 1000*(a))
+
+        # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
+        # (0,0,255) denotes the colour of the line to be 
+        #drawn. In this case, it is red.
+        cv2.line(imageEdges,(x1,y1), (x2,y2), (255,0,0), 1)
+
+    # All the changes made in the input image are finally
+    # written on a new image houghlines.jpg
+    return imageEdges, welderAngle
 
 def siftMatch(targetImage):
     # find the keypoints and descriptors with SIFT
@@ -66,6 +116,45 @@ def siftMatch(targetImage):
 
     plt.imshow(img3, 'gray'),plt.show()
 
+def sharpen(image):
+    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    res = cv2.filter2D(image, -1, kernel)
+    return res
+
+def grayQuantize(image, numValues):
+    # load the image and grab its width and height
+
+    # reshape the image into a feature vector so that k-means
+    # can be applied
+    image = image.reshape((image.shape[0] * image.shape[1], 1))
+
+    # apply k-means using the specified number of clusters and
+    # then create the quantized image based on the predictions
+    clt = MiniBatchKMeans(n_clusters = numValues)
+    # print(clt)
+    return clt
+
+def applyQuant(image, clt):
+    (h, w) = image.shape[:2]
+    image = image.reshape((image.shape[0] * image.shape[1], 1))
+
+    labels = clt.fit_predict(image)
+    quant = clt.cluster_centers_.astype("uint8")[labels]
+
+    # reshape the feature vectors to images
+    quant = quant.reshape((h, w, 1))
+
+    return quant
+
+def preProcess(image):
+    res = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # print(res.mean())
+    # create a CLAHE object (Arguments are optional).
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    res = clahe.apply(res)
+    
+    # gray = sharpen(gray)
+    return res
 
 def main():
     cap = cv2.VideoCapture('sample_weld_video.mp4')
@@ -75,31 +164,25 @@ def main():
     frameNum = 0
 
     ret, frame1 = cap.read()
-    height,width,depth = frame1.shape
+    frame1 = preProcess(frame1)
+    clt = grayQuantize(frame1, 4)
 
-    
-
-    
-
-
-    prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    height,width = frame1.shape
+    prvs = frame1
 
     while(cap.isOpened()):
 
         frameNum += 1
         ret, frame = cap.read()
         try:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # create a CLAHE object (Arguments are optional).
-            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-            gray = clahe.apply(gray)
+            gray = preProcess(frame)
+            # print(gray.mean())
         except Exception as e:
+            print("Error")
             break
 
         grayCopy = gray.copy()
-
-
-
+        # grayCopy = applyQuant(grayCopy, clt)
 
         kernel = np.ones((20,20),np.float32)/400
         gray = cv2.filter2D(gray,-1,kernel)
@@ -108,16 +191,18 @@ def main():
 
         imgDiff = mse(prvs, next)
         if imgDiff > 300:
-            print(imgDiff)
+            # print(imgDiff)
+            pass
 
         imgDiff2 = ssim(prvs, next)
         if imgDiff2 < .95:
-            print(imgDiff2)
+            # print(imgDiff2)
+            pass
 
         ret,thresh = cv2.threshold(gray, 140,255,cv2.THRESH_BINARY)
         threshcopy = thresh
 
-        # edges = cv2.Canny(grayCopy, 5, 40)
+        edges = cv2.Canny(grayCopy, 5, 100)
 
         imTemp, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
@@ -135,36 +220,40 @@ def main():
 
             ellipse = cv2.fitEllipse(contours[0])
 
-            welderRectangleCorner = np.asarray(ellipse[0])+np.asarray((ellipse[1][1]/2, 0))
-            welderRectangle = np.asarray((welderRectangleCorner, np.asarray((600, -200))+welderRectangleCorner))
+            welderRectangleCorner = np.asarray(ellipse[0])+np.asarray((ellipse[1][1]*0, 0))
+            welderRectangle = np.asarray((welderRectangleCorner, np.asarray((600, -200)) + welderRectangleCorner))
             welderRectangle = welderRectangle.astype(np.int64)
             welderRectangle = tuple(map(tuple, welderRectangle))
 
-            print(welderRectangle)
+            # print(welderRectangle)
 
             rect_img = np.zeros((height,width), np.uint8)
             cv2.rectangle(rect_img, welderRectangle[0], welderRectangle[1], (255,0,0), -1)
             masked_data = cv2.bitwise_and(grayCopy, grayCopy, mask=rect_img)
-            siftMatch(masked_data)
+            masked_data = applyQuant(masked_data, clt)
+            # siftMatch(masked_data)
             # laplacian = cv2.Laplacian(masked_data, cv2.CV_64F)
             # sobelx = cv2.Sobel(masked_data,cv2.CV_64F,0,1,ksize=5)
+
+            edges = cv2.Canny(masked_data, 5, 100)
 
             cv2.ellipse(thresh,ellipse,(255,0,0),2)
 
             hull = cv2.convexHull(contours[0])
 
-            cv2.drawContours(thresh, contours, -1, (255,255,0), 3)
-            cv2.drawContours(thresh, hull, -1, (255,255,0), 3)
-            cv2.imwrite('welderMask%d.png' %frameNum ,masked_data)
+            cv2.drawContours(thresh, contours, -1, (255,0,0), -2)
+            cv2.drawContours(thresh, hull, -1, (255,0,0), -2)
+            # cv2.imwrite('welderMask%d.png' %frameNum ,masked_data)
             # masked_data = cv2.bitwise_not(masked_data)
             cv2.imshow("Welder Mask", masked_data)
 
         res = cv2.add(thresh, grayCopy)
-        # res = cv2.add(res, edges)
-
+        res = cv2.add(res, edges)
+        imageEdges, welderAngle = detectLines(edges)
+        res = cv2.add(res, imageEdges)
 
         cv2.imshow("Contour", res)
-        
+
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
